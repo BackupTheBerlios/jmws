082bab2b629f2ca265de8f31323c0f311fca7582
mikaelbarbeaux <mikaelbarbeaux>
2004-03-25 10:13:36 +0000

    2004/03/25 Mikael Barbeaux 	* Added a Test class for UserCreation. 	* First revision of the project.

---
+++ jmws-tests/src/org/jmws/tests/user/creation/TestUserCreation.java
@@ -0,0 +1,61 @@
+package org.jmws.tests.user.creation;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import javax.naming.InitialContext;
+
+import org.jmws.session.user.creation.UserCreation;
+import org.jmws.session.user.creation.UserCreationHome;
+import org.jmws.session.user.creation.UserCreationRemote;
+
+/**
+ * TestUserCreation
+ * 
+ * @author Mikael Barbeaux
+ */
+public class TestUserCreation {
+
+	/**
+	 * Main program. Test for the creation of a User.
+	 * 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		try {
+			// JNDI naming context
+			InitialContext context  = new InitialContext();
+			
+			// Get UserCreationHome interface
+			UserCreationHome home;
+			Object obj = context.lookup(UserCreation.JNDI_NAME);
+			home = (UserCreationHome) obj;
+			
+			// Get UserCreationRemote interface
+			UserCreationRemote remote = home.create();
+			
+			// Asking for new User's login and password
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(System.in));
+			
+			System.out.print("Login: ");
+			String login = br.readLine();
+			System.out.print("Password: ");
+			String password = br.readLine();
+			
+			// Create the new User
+			remote.addUser(login, password, Boolean.FALSE);
+			System.out.println("-==== USER CREATED ====-");
+						
+			
+		}
+		catch(Exception e) {
+			System.out.println("Exception thrown...");
+			System.out.println("Exception ClassName: " + 
+				e.getClass().getName());
+			System.out.println("Message: " + e.getMessage());	
+		}
+	}
+}
